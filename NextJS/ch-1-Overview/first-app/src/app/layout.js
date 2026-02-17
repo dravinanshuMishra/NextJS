e8ad8b16c99c9d415ddb8dbade4f6653efe4a75b
dravinanshuMishra <dravinanshu97@gmail.com>
2026-02-17 11:35:44 +0530

    add

---
+++ NextJS/ch-1-Overview/first-app/src/app/layout.js
@@ -0,0 +1,37 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Dravinanshu Portfolio",
+  description: "Welcome to Dravinanshu Mishra Portfolio",
+  icons: {
+    icon: [
+      {
+        url: "/icon.png",
+        type: "image/png",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ NextJS/ch-1-Overview/first-app/src/app/readme.md
@@ -0,0 +1,3 @@
+app folder ka kaam hai: Routing, Layout, Pages, Loading, Error, Metadata (SEO)
+
+Business logic, components, redux etc app ke andar nahi likhte.
